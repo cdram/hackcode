3e1b638bdf89cea5b1270d8059998c7b39e1b21c
shriram <shriram.r.ramamurthy@gmail.com>
2015-10-20 23:16:02 -0700

    added problem descritpion

--- Trees/arrToTree.java
+++ Trees/arrToTree.java
@@ -1,3 +1,15 @@
+/***
+	Write a function that given an array representation of a binary tree will convert it into a typical tree format.
+	The following is a visual representation of expected input and output:
+
+	Input: [7, 3, 9, 2, 4, 8, 10]
+	Output:
+	7
+	/ \
+	3 9
+	/\ /\
+	2 4 8 10
+***/
 import java.util.*;
 import java.io.*;
 
